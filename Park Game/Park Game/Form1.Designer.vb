d7ee54682006ff642258b89aa48498bc6985c9fc
Programming Wiz <allisprogramming12789@gmail.com>
2026-03-18 03:23:29 +0700

    Initial commit

---
+++ Park Game/Park Game/Form1.Designer.vb
@@ -0,0 +1,389 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ParkGame
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cell00 = New Button()
+        cell01 = New Button()
+        cell02 = New Button()
+        cell03 = New Button()
+        cell04 = New Button()
+        cell06 = New Button()
+        cell05 = New Button()
+        cell12 = New Button()
+        cell11 = New Button()
+        cell10 = New Button()
+        cell16 = New Button()
+        cell15 = New Button()
+        cell14 = New Button()
+        cell13 = New Button()
+        cell20 = New Button()
+        cell21 = New Button()
+        cell22 = New Button()
+        cell23 = New Button()
+        cell24 = New Button()
+        cell25 = New Button()
+        cell26 = New Button()
+        cell31 = New Button()
+        cell30 = New Button()
+        cell32 = New Button()
+        cell35 = New Button()
+        cell34 = New Button()
+        cell33 = New Button()
+        cell36 = New Button()
+        restartBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' cell00
+        ' 
+        cell00.BackColor = Color.Red
+        cell00.Location = New Point(12, 12)
+        cell00.Name = "cell00"
+        cell00.Size = New Size(80, 80)
+        cell00.TabIndex = 0
+        cell00.UseVisualStyleBackColor = False
+        ' 
+        ' cell01
+        ' 
+        cell01.BackColor = Color.Red
+        cell01.Location = New Point(87, 12)
+        cell01.Name = "cell01"
+        cell01.Size = New Size(80, 80)
+        cell01.TabIndex = 1
+        cell01.UseVisualStyleBackColor = False
+        ' 
+        ' cell02
+        ' 
+        cell02.BackColor = Color.Red
+        cell02.Location = New Point(163, 12)
+        cell02.Name = "cell02"
+        cell02.Size = New Size(80, 80)
+        cell02.TabIndex = 2
+        cell02.UseVisualStyleBackColor = False
+        ' 
+        ' cell03
+        ' 
+        cell03.BackColor = Color.Yellow
+        cell03.Location = New Point(240, 12)
+        cell03.Name = "cell03"
+        cell03.Size = New Size(80, 80)
+        cell03.TabIndex = 3
+        cell03.UseVisualStyleBackColor = False
+        ' 
+        ' cell04
+        ' 
+        cell04.BackColor = Color.Yellow
+        cell04.Location = New Point(317, 12)
+        cell04.Name = "cell04"
+        cell04.Size = New Size(80, 80)
+        cell04.TabIndex = 4
+        cell04.UseVisualStyleBackColor = False
+        ' 
+        ' cell06
+        ' 
+        cell06.BackColor = Color.Yellow
+        cell06.Location = New Point(470, 12)
+        cell06.Name = "cell06"
+        cell06.Size = New Size(80, 80)
+        cell06.TabIndex = 6
+        cell06.UseVisualStyleBackColor = False
+        ' 
+        ' cell05
+        ' 
+        cell05.BackColor = Color.Yellow
+        cell05.Location = New Point(394, 12)
+        cell05.Name = "cell05"
+        cell05.Size = New Size(80, 80)
+        cell05.TabIndex = 5
+        cell05.UseVisualStyleBackColor = False
+        ' 
+        ' cell12
+        ' 
+        cell12.BackColor = Color.Red
+        cell12.Location = New Point(163, 88)
+        cell12.Name = "cell12"
+        cell12.Size = New Size(80, 80)
+        cell12.TabIndex = 9
+        cell12.UseVisualStyleBackColor = False
+        ' 
+        ' cell11
+        ' 
+        cell11.BackColor = Color.Red
+        cell11.Location = New Point(87, 88)
+        cell11.Name = "cell11"
+        cell11.Size = New Size(80, 80)
+        cell11.TabIndex = 8
+        cell11.UseVisualStyleBackColor = False
+        ' 
+        ' cell10
+        ' 
+        cell10.BackColor = Color.Red
+        cell10.Location = New Point(12, 88)
+        cell10.Name = "cell10"
+        cell10.Size = New Size(80, 80)
+        cell10.TabIndex = 7
+        cell10.UseVisualStyleBackColor = False
+        ' 
+        ' cell16
+        ' 
+        cell16.BackColor = Color.Yellow
+        cell16.Location = New Point(470, 88)
+        cell16.Name = "cell16"
+        cell16.Size = New Size(80, 80)
+        cell16.TabIndex = 12
+        cell16.UseVisualStyleBackColor = False
+        ' 
+        ' cell15
+        ' 
+        cell15.BackColor = Color.Yellow
+        cell15.Location = New Point(394, 88)
+        cell15.Name = "cell15"
+        cell15.Size = New Size(80, 80)
+        cell15.TabIndex = 11
+        cell15.UseVisualStyleBackColor = False
+        ' 
+        ' cell14
+        ' 
+        cell14.BackColor = Color.Yellow
+        cell14.Location = New Point(317, 88)
+        cell14.Name = "cell14"
+        cell14.Size = New Size(80, 80)
+        cell14.TabIndex = 10
+        cell14.UseVisualStyleBackColor = False
+        ' 
+        ' cell13
+        ' 
+        cell13.BackColor = Color.Green
+        cell13.Location = New Point(240, 88)
+        cell13.Name = "cell13"
+        cell13.Size = New Size(80, 80)
+        cell13.TabIndex = 13
+        cell13.UseVisualStyleBackColor = False
+        ' 
+        ' cell20
+        ' 
+        cell20.BackColor = Color.Red
+        cell20.Location = New Point(12, 165)
+        cell20.Name = "cell20"
+        cell20.Size = New Size(80, 80)
+        cell20.TabIndex = 14
+        cell20.UseVisualStyleBackColor = False
+        ' 
+        ' cell21
+        ' 
+        cell21.BackColor = Color.Blue
+        cell21.Location = New Point(87, 165)
+        cell21.Name = "cell21"
+        cell21.Size = New Size(80, 80)
+        cell21.TabIndex = 15
+        cell21.UseVisualStyleBackColor = False
+        ' 
+        ' cell22
+        ' 
+        cell22.BackColor = Color.Blue
+        cell22.Location = New Point(163, 165)
+        cell22.Name = "cell22"
+        cell22.Size = New Size(80, 80)
+        cell22.TabIndex = 16
+        cell22.UseVisualStyleBackColor = False
+        ' 
+        ' cell23
+        ' 
+        cell23.BackColor = Color.Green
+        cell23.Location = New Point(240, 165)
+        cell23.Name = "cell23"
+        cell23.Size = New Size(80, 80)
+        cell23.TabIndex = 17
+        cell23.UseVisualStyleBackColor = False
+        ' 
+        ' cell24
+        ' 
+        cell24.BackColor = Color.Green
+        cell24.Location = New Point(317, 165)
+        cell24.Name = "cell24"
+        cell24.Size = New Size(80, 80)
+        cell24.TabIndex = 18
+        cell24.UseVisualStyleBackColor = False
+        ' 
+        ' cell25
+        ' 
+        cell25.BackColor = Color.Green
+        cell25.Location = New Point(394, 165)
+        cell25.Name = "cell25"
+        cell25.Size = New Size(80, 80)
+        cell25.TabIndex = 19
+        cell25.UseVisualStyleBackColor = False
+        ' 
+        ' cell26
+        ' 
+        cell26.BackColor = Color.Yellow
+        cell26.Location = New Point(470, 165)
+        cell26.Name = "cell26"
+        cell26.Size = New Size(80, 80)
+        cell26.TabIndex = 20
+        cell26.UseVisualStyleBackColor = False
+        ' 
+        ' cell31
+        ' 
+        cell31.BackColor = Color.Blue
+        cell31.Location = New Point(88, 241)
+        cell31.Name = "cell31"
+        cell31.Size = New Size(80, 80)
+        cell31.TabIndex = 22
+        cell31.UseVisualStyleBackColor = False
+        ' 
+        ' cell30
+        ' 
+        cell30.BackColor = Color.Blue
+        cell30.Location = New Point(12, 241)
+        cell30.Name = "cell30"
+        cell30.Size = New Size(80, 80)
+        cell30.TabIndex = 21
+        cell30.UseVisualStyleBackColor = False
+        ' 
+        ' cell32
+        ' 
+        cell32.BackColor = Color.Blue
+        cell32.Location = New Point(163, 241)
+        cell32.Name = "cell32"
+        cell32.Size = New Size(80, 80)
+        cell32.TabIndex = 23
+        cell32.UseVisualStyleBackColor = False
+        ' 
+        ' cell35
+        ' 
+        cell35.BackColor = Color.Green
+        cell35.Location = New Point(394, 241)
+        cell35.Name = "cell35"
+        cell35.Size = New Size(80, 80)
+        cell35.TabIndex = 26
+        cell35.UseVisualStyleBackColor = False
+        ' 
+        ' cell34
+        ' 
+        cell34.BackColor = Color.Green
+        cell34.Location = New Point(317, 241)
+        cell34.Name = "cell34"
+        cell34.Size = New Size(80, 80)
+        cell34.TabIndex = 25
+        cell34.UseVisualStyleBackColor = False
+        ' 
+        ' cell33
+        ' 
+        cell33.BackColor = Color.Green
+        cell33.Location = New Point(240, 241)
+        cell33.Name = "cell33"
+        cell33.Size = New Size(80, 80)
+        cell33.TabIndex = 24
+        cell33.UseVisualStyleBackColor = False
+        ' 
+        ' cell36
+        ' 
+        cell36.BackColor = Color.Green
+        cell36.Location = New Point(470, 241)
+        cell36.Name = "cell36"
+        cell36.Size = New Size(80, 80)
+        cell36.TabIndex = 27
+        cell36.UseVisualStyleBackColor = False
+        ' 
+        ' restartBtn
+        ' 
+        restartBtn.BackColor = SystemColors.Control
+        restartBtn.Font = New Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        restartBtn.Location = New Point(573, 124)
+        restartBtn.Name = "restartBtn"
+        restartBtn.Size = New Size(90, 90)
+        restartBtn.TabIndex = 28
+        restartBtn.Text = "Restart"
+        restartBtn.UseVisualStyleBackColor = False
+        ' 
+        ' ParkGame
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.SaddleBrown
+        ClientSize = New Size(685, 338)
+        Controls.Add(restartBtn)
+        Controls.Add(cell36)
+        Controls.Add(cell35)
+        Controls.Add(cell34)
+        Controls.Add(cell33)
+        Controls.Add(cell32)
+        Controls.Add(cell31)
+        Controls.Add(cell30)
+        Controls.Add(cell26)
+        Controls.Add(cell25)
+        Controls.Add(cell24)
+        Controls.Add(cell23)
+        Controls.Add(cell22)
+        Controls.Add(cell21)
+        Controls.Add(cell20)
+        Controls.Add(cell13)
+        Controls.Add(cell16)
+        Controls.Add(cell15)
+        Controls.Add(cell14)
+        Controls.Add(cell12)
+        Controls.Add(cell11)
+        Controls.Add(cell10)
+        Controls.Add(cell06)
+        Controls.Add(cell05)
+        Controls.Add(cell04)
+        Controls.Add(cell03)
+        Controls.Add(cell02)
+        Controls.Add(cell01)
+        Controls.Add(cell00)
+        Name = "ParkGame"
+        Text = "Park Game"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents cell00 As Button
+    Friend WithEvents cell01 As Button
+    Friend WithEvents cell02 As Button
+    Friend WithEvents cell03 As Button
+    Friend WithEvents cell04 As Button
+    Friend WithEvents cell06 As Button
+    Friend WithEvents cell05 As Button
+    Friend WithEvents cell12 As Button
+    Friend WithEvents cell11 As Button
+    Friend WithEvents cell10 As Button
+    Friend WithEvents cell16 As Button
+    Friend WithEvents cell15 As Button
+    Friend WithEvents cell14 As Button
+    Friend WithEvents cell13 As Button
+    Friend WithEvents cell20 As Button
+    Friend WithEvents cell21 As Button
+    Friend WithEvents cell22 As Button
+    Friend WithEvents cell23 As Button
+    Friend WithEvents cell24 As Button
+    Friend WithEvents cell25 As Button
+    Friend WithEvents cell26 As Button
+    Friend WithEvents cell31 As Button
+    Friend WithEvents cell30 As Button
+    Friend WithEvents cell32 As Button
+    Friend WithEvents cell35 As Button
+    Friend WithEvents cell34 As Button
+    Friend WithEvents cell33 As Button
+    Friend WithEvents cell36 As Button
+    Friend WithEvents restartBtn As Button
+
+End Class
---
+++ Park Game/Park Game/Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class ParkGame
+    Dim TREE As Image = Image.FromFile("Tree.gif")
+    Dim ROWS As Integer = 4, COLUMNS As Integer = 7, REGIONS As Integer = 4
+    Dim PARK(,) As Integer = {
+        {0, 0, 0, 2, 2, 2, 2},
+        {0, 0, 0, 3, 2, 2, 2},
+        {0, 1, 1, 3, 3, 3, 2},
+        {1, 1, 1, 3, 3, 3, 3}
+    }
+    Dim cells(ROWS, COLUMNS) As Button
+    Dim R(ROWS), C(COLUMNS), D1(ROWS + COLUMNS - 1), D2(ROWS + COLUMNS - 1), RG(REGIONS) As Boolean
+    Dim trees(ROWS, COLUMNS), gameOver As Boolean
+    Dim treesCnt As Integer
+
+    Private Sub ParkGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i = 0 To ROWS - 1
+            For j = 0 To COLUMNS - 1
+                cells(i, j) = DirectCast(Controls.Find("cell" & i & j, False).First, Button)
+                trees(i, j) = False
+            Next
+        Next
+        For i = 0 To ROWS - 1
+            R(i) = True
+        Next
+        For i = 0 To COLUMNS - 1
+            C(i) = True
+        Next
+        For i = 0 To ROWS + COLUMNS - 2
+            D1(i) = True
+            D2(i) = True
+        Next
+        For i = 0 To REGIONS - 1
+            RG(i) = True
+        Next
+        treesCnt = 0
+        gameOver = False
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles cell36.Click, cell35.Click, cell34.Click, cell33.Click, cell32.Click, cell31.Click, cell30.Click, cell26.Click, cell25.Click, cell24.Click, cell23.Click, cell22.Click, cell21.Click, cell20.Click, cell13.Click, cell16.Click, cell15.Click, cell14.Click, cell12.Click, cell11.Click, cell10.Click, cell06.Click, cell05.Click, cell04.Click, cell03.Click, cell02.Click, cell01.Click, cell00.Click
+        If Not gameOver Then
+            Dim cellName As String = CType(sender, Button).Name
+            Dim cellNum As Integer = CType(cellName.Substring(4), Integer)
+            Dim row As Integer = cellNum \ 10, col As Integer = cellNum Mod 10
+            Dim region As Integer = PARK(row, col)
+            If R(row) And C(col) And D1((COLUMNS - 1) + row - col) And D2(row + col) And RG(region) Then
+                cells(row, col).Image = TREE
+                trees(row, col) = True
+                R(row) = False
+                C(col) = False
+                D1((COLUMNS - 1) + row - col) = False
+                D2(row + col) = False
+                RG(region) = False
+                treesCnt += 1
+            ElseIf trees(row, col) Then
+                cells(row, col).Image = Nothing
+                trees(row, col) = False
+                R(row) = True
+                C(col) = True
+                D1((COLUMNS - 1) + row - col) = True
+                D2(row + col) = True
+                RG(region) = True
+                treesCnt -= 1
+            Else
+                MessageBox.Show("Occupied")
+            End If
+            If treesCnt = REGIONS Then
+                gameOver = True
+                MessageBox.Show("Solved!")
+            End If
+        End If
+    End Sub
+
+    Private Sub restartBtn_Click(sender As Object, e As EventArgs) Handles restartBtn.Click
+        For i = 0 To ROWS - 1
+            For j = 0 To COLUMNS - 1
+                cells(i, j).Image = Nothing
+                trees(i, j) = False
+            Next
+        Next
+        For i = 0 To ROWS - 1
+            R(i) = True
+        Next
+        For i = 0 To COLUMNS - 1
+            C(i) = True
+        Next
+        For i = 0 To ROWS + COLUMNS - 2
+            D1(i) = True
+            D2(i) = True
+        Next
+        For i = 0 To REGIONS - 1
+            RG(i) = True
+        Next
+        treesCnt = 0
+        gameOver = False
+    End Sub
+End Class
